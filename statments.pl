df929a645b1d43598ea866b24f38ed102e24cf51
Gabriel Mantilla Saltos <36687480+gabofreq@users.noreply.github.com>
2021-06-20 18:31:39 -0500

    Add files via upload

---
+++ statments.pl
@@ -0,0 +1,14 @@
+vector_type(double(_List), 2).
+vector_type(float(_List), 3).
+vector_type(integer(_List), 4).
+vector_type(integer64(_List), 5).
+vector_type(integer32(_List), 6).
+vector_type(unsigned(_List), 7).
+vector_type(codes(_List), 8).
+vector_type(atom(_List), 9).
+vector_type(string(_List), 10).
+
+vector(Type, B):-
+    vector_type(Type, Tag),
+    Proto = protobuf([ repeated(Tag, Type) ]),
+    protobuf_message(Proto, B).
