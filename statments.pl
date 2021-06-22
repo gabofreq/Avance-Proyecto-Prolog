72c768c68a820b13b5df035ed0abc8c84b7f7ece
Gabriel Mantilla Saltos <36687480+gabofreq@users.noreply.github.com>
2021-06-21 23:46:24 -0500

    Update statments.pl

--- statments.pl
+++ statments.pl
@@ -78,3 +78,90 @@ ciclo_for(entrada, salida) :-
     nl,
     N is entrada + 1,
     ciclo_for(N, salida). 
+
+% simulador data frame de R
+r_data_frame(RVar, ColSpec, Goal) :-
+	must_be(atom, RVar),
+   	maplist(arg(1), ColSpec, Names),
+   	maplist(arg(2), ColSpec, Vars),
+   	Templ =.. [v|Vars],
+   	findall(Templ, Goal, Rows),
+   	r_data_frame_from_rows(RVar, Rows),
+   	colnames(RVar) <- Names.
+
+r_data_frame_to_dicts(DataFrame, Dicts) :-
+	Cols <- DataFrame,
+	ColNameStrings <- colnames(DataFrame),
+	maplist(atom_string, ColNames, ColNameStrings),
+	pairs_keys_values(Pairs, ColNames, _),
+	dict_pairs(Templ, _, Pairs),
+	maplist(dict_cols(Templ, Dicts), ColNames, Cols).
+
+dict_cols(Templ, Dicts, Name, Col) :-
+ 	maplist(fill_col(Templ, Name), Col, Dicts).
+
+fill_col(_, Name, Value, Dict) :-
+	nonvar(Dict), !,
+	get_dict(Name, Dict, Value).
+
+fill_col(Templ, Name, Value, Dict) :-
+	copy_term(Templ, Dict),
+	get_dict(Name, Dict, Value).
+
+r_data_frame_to_rows(DataFrame, Functor, Rows) :-
+	Cols <- DataFrame,
+	length(Cols, Arity),
+	term_cols(Cols, 1, Arity, Functor, Rows).
+
+term_cols([], _, _, _, _).
+term_cols([Col|Cols], I, Arity, Functor, Rows) :-
+	maplist(term_col(I, Arity, Functor), Col, Rows),
+	I2 is I+1,
+	term_cols(Cols, I2, Arity, Functor, Rows).
+
+term_col(1, Arity, Functor, Value, Term) :- !,
+	functor(Term, Functor, Arity),
+	arg(1, Term, Value).
+
+6term_col(I, _, _, Value, Term) :-
+	arg(I, Term, Value).
+
+r_data_frame_from_dicts(DataFrame, Rows) :-
+	must_be(atom, DataFrame),
+	must_be(list, Rows),
+	Rows = [Row1|_],
+	dict_keys(Row1, Keys),
+	dict_col_data(Keys, Rows, ColData),
+	compound_name_arguments(Term, 'data.frame', ColData),
+	DataFrame <- Term,
+	colnames(DataFrame) <- Keys.
+
+dict_col_data([], _, []).
+dict_col_data([K|Keys], Rows, [ColI|ColR]) :-
+	maplist(get_dict(K), Rows, ColI),
+	dict_col_data(Keys, Rows, ColR).
+
+r_data_frame_from_rows(DataFrame, Rows) :-
+	must_be(atom, DataFrame),
+	must_be(list, Rows),
+	Rows = [Row1|_],
+	functor(Row1, _, NCols),
+	col_data(1, NCols, Rows, ColData),
+	append(ColData, [stringsAsFactors = 'FALSE'], ColDataOpts),
+	compound_name_arguments(Term, 'data.frame', ColDataOpts),
+	DataFrame <- Term.
+
+col_data(I, NCols, Rows, [ColI|ColR]) :-
+	I =< NCols, !,
+	maplist(arg(I), Rows, ColI),
+	I2 is I + 1,
+	col_data(I2, NCols, Rows, ColR).
+col_data(_, _, _, []).
+
+r_data_frame_colnames(DataFrame, ColNames) :-
+	ColNameStrings <- colnames(DataFrame),
+	maplist(atom_string, ColNames, ColNameStrings).
+
+r_data_frame_rownames(DataFrame, RowNames) :-
+	RowNameStrings <- rownames(DataFrame),
+	maplist(atom_string, RowNames, RowNameStrings).
